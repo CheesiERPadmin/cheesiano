7d7a9c7ab13a5aa4c05b6ff5673dac1ac3ba92c6
PratikMane34 <manepratik7@gmail.com>
2023-09-11 16:27:24 +0530

    Item supplier is not available case handled

--- cheesiano/cheesiano/custom_script/material_request.py
+++ cheesiano/cheesiano/custom_script/material_request.py
@@ -5,8 +5,15 @@ def create_purchase_order(doc,method):
     if doc.material_request_type == "Purchase":
         po_doc = make_purchase_order(doc.name)
         for item in doc.items:
-            po_doc.supplier = frappe.db.get_value("Item Supplier",{"parent":item.item_code},'supplier') 
-            po_doc.save()
+            try:
+                po_doc.supplier = frappe.db.get_value("Item Supplier",{"parent":item.item_code},'supplier')
+                
+                if not po_doc.supplier:
+                    frappe.msgprint("Supplier is not assigned")
+                    po_doc.save()
+            except:
+                raise
+            
 
 def delete_purchase_order(doc,method):
     frappe.msgprint(f"Purchase Order for this Material Request {doc.name} has been deleted")
