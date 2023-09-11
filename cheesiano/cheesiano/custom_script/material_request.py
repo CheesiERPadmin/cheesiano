0523e3d6adf2f5f89c568030ddee24a03be125bc
PratikMane34 <manepratik7@gmail.com>
2023-09-11 15:15:20 +0530

    removed debuggers

--- cheesiano/cheesiano/custom_script/material_request.py
+++ cheesiano/cheesiano/custom_script/material_request.py
@@ -2,20 +2,12 @@ import frappe
 from erpnext.stock.doctype.material_request.material_request import make_purchase_order
 
 def create_purchase_order(doc,method):
-    # print(doc.item)
     if doc.material_request_type == "Purchase":
         po_doc = make_purchase_order(doc.name)
         for item in doc.items:
-            print("====Item details\n")
-            # print(item.name)
-            print(item.item_code) 
-            # item_info = frappe.get_doc("Item",item.item_code)
-            # print(f"item doctype fetched {item_info}")        
-            print("Create Purchase order function called after submission of materialrequest ")
             po_doc.supplier = frappe.db.get_value("Item Supplier",{"parent":item.item_code},'supplier') 
             po_doc.save()
 
 def delete_purchase_order(doc,method):
-    print("======= Material request is cancelled =======")
     frappe.msgprint(f"Purchase Order for this Material Request {doc.name} has been deleted")
     return True
