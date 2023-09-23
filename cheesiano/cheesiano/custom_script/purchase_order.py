0565b2dac11ddda5f36bc819fbba33186709b655
PratikMane34 <manepratik7@gmail.com>
2023-09-23 21:14:40 +0530

    Updates in Purchase order item price

--- cheesiano/cheesiano/custom_script/purchase_order.py
+++ cheesiano/cheesiano/custom_script/purchase_order.py
@@ -4,12 +4,10 @@ def save_item_price(doc,method):
     for item in doc.items:
         if item.rate != item.last_purchase_rate:
             item_price = frappe.get_doc(
-			{
-				"doctype": "Item Price",
-				"item_code": item.item_code,
-                "price_list": "Standard Buying",
-		    }
-		    )
+                "Item Price",{
+                    "item_code":item.item_code,
+                    "price_list":"Standard Buying"
+                    })
             item_price.price_list_rate = item.rate
-            item_price.insert()
+            item_price.save()
             frappe.db.commit()
